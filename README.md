0b71e7c95e58ab6f8074ff4f3d66f03781737f6a
Cedric Porter <cedricporter@gmail.com>
2013-02-01 23:39:41 +0800

    new

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 首先声明，我非常喜欢豆瓣fm以及依赖它。尽管大部分时间我都可以在有网络的环境下进行工作，但是，有时会去到没有网络的地方。但是我还是想听一下我的红心音乐。于是我就想下载我的红心音乐。
 
-尽管现在已经有很多红心音乐的下载器，不过它们都是通过<http://douban.fm/mine?type=liked>来获取红心列表，在根据列表进行下载。通过这样下载的音乐都是64kbps的码率，压缩太大了。
+尽管现在已经有很多红心音乐的下载器，不过它们都是通过<http://douban.fm/mine?type=liked>来获取红心列表，再根据列表进行下载。通过这样下载的音乐都是64kbps的码率，压缩太大了。
 
 我开通了douban.fm Pro，发现192kbps码率的音乐听起来确实比64kbps的好很多。于是我需要下载192kbps的mp3，以便我在没网络的时候也能够听音乐。
 
@@ -12,7 +12,7 @@
 
 ## 需求
 
-首先你需要一个Chrome或者Firefox，然后安装插件[豆瓣FM-Hacker](https://chrome.google.com/webstore/detail/豆瓣fm-hacker/pjpdhffcchclaedfnlkmpacghddkpgjh)。我的工作大部分是直接基于这个插件。懒得重复造轮子，直接站在巨人的肩膀上。
+首先你需要一个Chrome或者Firefox.
 
 ## 使用
 
--- download.js
+++ download.js
@@ -1,18 +1,22 @@
 var download_timeout, current_song_number = 0;
-var songs_number = 200;
+var songs_number = 500;
+var last_size = -1;
 
 function go()
 {
     try
     {
         window.DBR.act("skip");     // next song
-        var links = $(".saveBtn");
-        links[links.length - 1].click(); // download
-        console.log("Current song number: " + ++current_song_number);
-        if (current_song_number < songs_number)
+        var len = Object.keys(links_json).length;
+        if (last_size === len)
         {
-            download_timeout = setTimeout(go, 30 * 1000);
+            console.log("## Stopped ##");
+            return;
         }
+        console.log("Current list size: " + len);
+        last_size = len;
+
+        download_timeout = setTimeout(go, 5 * 1000);
     }
     catch (err)
     {
@@ -26,5 +30,24 @@ function stop()
     clearTimeout(download_timeout);
 }
 
-go();
+function download_json()
+{
+    $("<a href='" + "data:application/octet-stream," + encodeURIComponent(JSON.stringify(links_json)) + "' download='list.json'/>")[0].click();
+}
 
+
+// Thanks to `douban.fm Hacker`
+// hook to douban.fm's handler
+var extStatusHandlerBak = window.extStatusHandler;
+var links_json = {};
+window.extStatusHandler = function(a) {
+    extStatusHandlerBak(a);
+    var o = eval('(' + a + ')');
+    if (o.type == 'start') {
+        var s = o.song;
+        var name = s.title + ".mp3";
+        links_json[name] = s.url;
+    }
+}
+
+go();
